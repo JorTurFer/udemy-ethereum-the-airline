eaceae91e6d16f67f19a69d9beff7730c390443f
JorTurFer <jorge_turrado@hotmail.es>
2019-01-27 19:30:00 +0100

    Añado metodos al contrato

--- contracts/Airline.sol
+++ contracts/Airline.sol
@@ -13,10 +13,30 @@ contract Airline{
     }
 
     Flight[] public flights;
+
+    mapping(address => Customer) public customers;
+    mapping(address => Flight[]) public customerFlights;
+    mapping(address => uint) public sustomerTotalFlights;
+
+    event FlightPurchased(address indexed customer,uint price);
+
     constructor(){
         owner = msg.sender;
         flights.push(Flight("Tokio", 4 ether));
         flights.push(Flight("Germany", 1 ether));
         flights.push(Flight("Madrid", 2 ether));
+    }
+
+    function buyFlight(uint flightIndex) public payable{
+        Flight flight = flights[flightIndex];
+        require(msg.value == flight.price);
+
+        Customer storage customer = customers[msg.sender];
+        customer.loyaltyPoints += 5;
+        customer.totalFlights += 1;
+        customerFlights[msg.sender].push(flight);
+        sustomerTotalFlights[msg.sender] ++;
+
+        FlightPurchased(msg.sender,flight.price);
     }
 }
