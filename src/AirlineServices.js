65a4edb2d475a9f1ffd7c10f969e38bc65a73857
JorTurFer <jorge_turrado@hotmail.es>
2019-01-27 23:08:39 +0100

    Añado todo lo que falta

--- src/AirlineServices.js
+++ src/AirlineServices.js
@@ -1,44 +1,53 @@
-
-export class AirlineService{
-    constructor(contract){
+export class AirlineService {
+    constructor(contract) {
         this.contract = contract;
     }
 
-    async buyFlight(flightIndex,from,value){
-        return this.contract.buyFlight(flightIndex,{from,value});
+    async buyFlight(flightIndex, from, value) {
+        return this.contract.buyFlight(flightIndex, { from, value });
     }
 
-    async getFlights(){
+    async getFlights() {
         let total = await this.getTotalFlights();
         let flights = [];
-        for(var i = 0; i < total; i++){
+        for (var i = 0; i < total; i++) {
             let flight = await this.contract.flights(i);
             flights.push(flight);
         }
+
         return this.mapFlights(flights);
     }
 
-    async getTotalFlights(){
+    async getCustomerFlights(account) {
+
+        let customerTotalFlights = await this.contract.customerTotalFlights(account);
+        let flights = [];
+        for (var i = 0; i < customerTotalFlights.toNumber(); i++) {
+            let flight = await this.contract.customerFlights(account, i);
+            flights.push(flight);
+        }
+
+        return this.mapFlights(flights);
+    }
+
+    async getTotalFlights() {
         return (await this.contract.totalFlights()).toNumber();
     }
 
-    async getCustomerFlights(account){
-        let customerTotalFlights = await this.contract.customerTotalFlights(account);
-        let flights = [];
-        for(var i = 0; i < customerTotalFlights.toNumber(); i++){
-            let flight = await this.contract.customerFlights(account,i);
-            flights.push(flight);
-        }
-        return this.mapFlights(flights);
+    getRefundableEther(from) {
+        return this.contract.getRefoundableEther({ from });
     }
 
-    mapFlights(flights){
-        return flights.map(flight =>{
-            return{
+    redeemLoyaltyPoints(from) {
+        return this.contract.redeemLoyaltyPoints({ from });
+    }
+
+    mapFlights(flights) {
+        return flights.map(flight => {
+            return {
                 name: flight[0],
                 price: flight[1].toNumber()
             }
-        })
+        });
     }
-
 }
